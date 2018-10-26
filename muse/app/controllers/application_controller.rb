0e060a3bbf016d835dc546a73ecc657316b60123
Rafa <rafa@pop-os.localdomain>
2018-10-25 23:34:55 -0600

    comments

--- muse/app/controllers/application_controller.rb
+++ muse/app/controllers/application_controller.rb
@@ -1,14 +1,25 @@
 class ApplicationController < ActionController::Base
-     # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-#   protect_from_forgery with: :exception
-
- # before_action :configure_permitted_parameters, if: :devise_controller?
-
-#	protected
-
-#	def configure_permitted_parameters
-#	  devise_parameter_sanitizer.for(:sign_up) << :name
-#	  devise_parameter_sanitizer.for(:account_update) << :name
-#	end
+  #Prevent CSRF attacks by raising an exception.
+  #For APIs, you may want to use :null_session instead.
+  #rotect_from_forgery with: :null_session
+  #skip_before_action :verify_authenticity_token, :only => :create
+  protect_from_forgery 
 end
+
+
+
+
+
+
+
+#skip_before_action :verify_authenticity_token, :only => :create
+#=begin protect_from_forgery with: :exception
+
+#before_action :configure_permitted_parameters, if: :devise_controller?
+
+#protected
+
+#def configure_permitted_parameters
+
+#devise_parameter_sanitizer.for(:sign_up) << :name 
+#devise_parameter_sanitizer.for(:account_update) << :name =end
