51e5638d5aec4b0388e8fcfa850a149390036909
xipeRafa <rafaelsistem87@gmail.com>
2018-10-25 19:26:32 -0700

    paperclip

--- muse/app/models/post.rb
+++ muse/app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
+
+    has_attached_file :image, styles: { medium: "700x550", small: "400x350" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
