0531719f8f4f55368de12c55a2dce27f89fe62e1
xipeRafa <43357824+xipeRafa@users.noreply.github.com>
2018-11-02 05:04:41 -0700

    Update application_controller.rb

--- muse/app/controllers/application_controller.rb
+++ muse/app/controllers/application_controller.rb
@@ -1,21 +1,15 @@
 class ApplicationController < ActionController::Base
-  #Prevent CSRF attacks by raising an exception.
-  #For APIs, you may want to use :null_session instead.
-  #rotect_from_forgery with: :null_session
-  #skip_before_action :verify_authenticity_token, :only => :create
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-protected
-
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up) do |u|
-    u.permit(:name, :email, :password, :password_confirmation)
-  end
-  devise_parameter_sanitizer.permit(:account_update) do |u|
-    u.permit(:name, :email, :password, :password_confirmation, :current_password)
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
-end
 
 
 
@@ -23,14 +17,3 @@ end
 
 
 
-#skip_before_action :verify_authenticity_token, :only => :create
-#=begin protect_from_forgery with: :exception
-
-#before_action :configure_permitted_parameters, if: :devise_controller?
-
-#protected
-
-#def configure_permitted_parameters
-
-#devise_parameter_sanitizer.for(:sign_up) << :name 
-#devise_parameter_sanitizer.for(:account_update) << :name =end
